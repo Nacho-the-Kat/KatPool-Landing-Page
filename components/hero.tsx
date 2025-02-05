76b8d3836e102a1a70b0b0ba91283af8c7d20ad0
argonmining <ashton@onargon.com>
2025-02-04 21:48:35 -0800

    fix link

--- components/hero.tsx
+++ components/hero.tsx
@@ -24,7 +24,7 @@ export default function Hero() {
                   Kat Pool is open source.{' '}
                   <a
                     className="font-medium text-customPrimary inline-flex items-center transition duration-150 ease-in-out group"
-                    href="https://github.com/Nacho-the-Kat/katpool-payment"
+                    href="https://github.com/Nacho-the-Kat/katpool-app"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
